c97e95ffe11fc519b955285d03749b7a904204ac
darrenbates96 <i.am.darrenbates@gmail.com>
2021-02-02 05:11:24 +0200

    Update

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 8081;
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        console.log(roomId, userId);
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+
+        socket.on("disconnect", () => {
+            socket.to(roomId).broadcast.emit("user-disconnected", userId);
+        });
+    });
+});
+
+server.listen(port);
