eab3a23f7cf2da822c08e24028d59f5b880ab7c4
Ethan Jansen <jansenethan06@gmail.com>
2021-02-02 07:00:46 +0200

    Update

--- server.js
+++ server.js
@@ -2,7 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors({ origin: "*" }));
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    // Pass to next layer of middleware
+    next();
+});
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 8081;
