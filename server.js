6d01ddb418210efd92f7a73550c3515bfce529b8
Ethan Jansen <jansenethan06@gmail.com>
2021-02-02 06:31:15 +0200

    Update

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
 const app = express();
+app.use(cors());
+
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 8081;
